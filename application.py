1aa013cb588e4f5f529a1544c408bb10a9d87782
helton.silveira <helton.silveira@MacBook-Pro.local>
2020-07-30 20:35:05 -0300

    altera -version

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>Hola FIAP!</h1>\nMBA! o/"
+    return "<h1>Hola FIAP! Helton V@</h1>\nMBA! o/"
 
 if __name__ == '__main__':
     application.run()
